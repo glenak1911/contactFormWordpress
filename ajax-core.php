297d8cc8ef0608c48de53fa419d42bf3b7b687bc
Glen Knight <glenak1911@gmail.com>
2017-06-17 16:31:37 -0400

    Added mobile styling to input boxes

--- ajax-core.php
+++ ajax-core.php
@@ -18,15 +18,15 @@ function html_form_code(){
     echo '<form class="contactForm" action="' . esc_url( $_SERVER['REQUEST_URI'] ) . '" method="post">';
   	echo '<p>';
   	echo 'Your Name (required) <br/>';
-  	echo '<input type="text" name="cf-name" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" required />';
+  	echo '<input class="custom-text" type="text" name="cf-name" value="' . ( isset( $_POST["cf-name"] ) ? esc_attr( $_POST["cf-name"] ) : '' ) . '" size="40" required />';
   	echo '</p>';
   	echo '<p>';
   	echo 'Your Email (required) <br/>';
-  	echo '<input type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" required />';
+  	echo '<input class="custom-text" type="email" name="cf-email" value="' . ( isset( $_POST["cf-email"] ) ? esc_attr( $_POST["cf-email"] ) : '' ) . '" size="40" required />';
   	echo '</p>';
   	echo '<p>';
   	echo 'Subject (required) <br/>';
-  	echo '<input type="text" name="cf-subject" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" required />';
+  	echo '<input class="custom-text" type="text" name="cf-subject" value="' . ( isset( $_POST["cf-subject"] ) ? esc_attr( $_POST["cf-subject"] ) : '' ) . '" size="40" required />';
   	echo '</p>';
   	echo '<p>';
   	echo 'Your Message (required) <br/>';
